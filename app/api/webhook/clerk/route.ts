50979d556f3cf39d6f9dacd1fa14b9022bae0f25
Shahzaib <shaulazain@gmail.com>
2024-04-22 15:32:58 +0500

    second commit

--- app/api/webhook/clerk/route.ts
+++ app/api/webhook/clerk/route.ts
@@ -60,8 +60,8 @@ export async function POST(req: Request) {
       clerkId: id,
       email: email_addresses[0].email_address,
       username: username!,
-      firstName: first_name,
-      lastName: last_name,
+      firstName: first_name!,
+      lastName: last_name!,
       photo: image_url,
     }
 
@@ -82,10 +82,10 @@ export async function POST(req: Request) {
     const {id, image_url, first_name, last_name, username } = evt.data
 
     const user = {
-      firstName: first_name,
-      lastName: last_name,
+      firstName: first_name!,
+      lastName: last_name!,
       username: username!,
-      photo: image_url,
+      photo: image_url!,
     }
 
     const updatedUser = await updateUser(id, user)
