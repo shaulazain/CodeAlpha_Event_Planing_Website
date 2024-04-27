309f8b6c1f1628c509847d68e5aa31f3fa5e7395
Shahzaib <shaulazain@gmail.com>
2024-04-27 15:29:55 +0500

    finish the app

--- lib/database/models/event.model.ts
+++ lib/database/models/event.model.ts
@@ -1,19 +1,19 @@
 import { Document, Schema, model, models } from "mongoose";
 
 export interface IEvent extends Document {
-    _id: string;
-    title: string;
-    description?: string;
-    location?: string;
-    createdAt: Date;
-    imageUrl: string;
-    startDateTime: Date;
-    endDateTime: Date;
-    price: string;
-    isFree: boolean;
-    url?: string;
-    category: { _id: string, name: string }
-    organizer: { _id: string, firstName: string, lastName: string }
+  _id: string;
+  title: string;
+  description?: string;
+  location?: string;
+  createdAt: Date;
+  imageUrl: string;
+  startDateTime: Date;
+  endDateTime: Date;
+  price: string;
+  isFree: boolean;
+  url?: string;
+  category: { _id: string, name: string }
+  organizer: { _id: string, firstName: string, lastName: string }
 }
 
 const EventSchema = new Schema({
